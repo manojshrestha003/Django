dcd75db1675255520a06a1058fd0f60058a46f35
manojshrestha003 <manoj.shrestha2081@gmail.com>
2025-01-08 09:46:02 +0545

    creating and connectind database

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):  # Corrected `models.model` to `models.Model`
+    email = models.CharField(max_length=122)
+    password = models.CharField(max_length=8)  # Password field (consider using Django's built-in user model for better security)
+    address1 = models.CharField(max_length=25)
+    address2 = models.CharField(max_length=25)
+    city = models.CharField(max_length=25)
+
+   
--- myapp/views.py
+++ myapp/views.py
@@ -1,21 +1,37 @@
 from django.shortcuts import render, HttpResponse
+from myapp.models import Contact
 
 # Create your views here.
+
 def index(request):
     context = {
         "variable": "Manoj Shrestha"
     }
     return render(request, 'index.html', context)
-    #return HttpResponse("This  is Home Page ")
+    # return HttpResponse("This  is Home Page")
+
 def about(request):
-   # return HttpResponse("This  is about Page ")
+    # return HttpResponse("This  is about Page")
     return render(request, 'about.html')
 
 def services(request):
-   # return HttpResponse("This  is services Page ")
-     return render(request, 'services.html')
+    # return HttpResponse("This  is services Page")
+    return render(request, 'services.html')
+
 def contact(request):
-    #return HttpResponse("<h1>Hello  this is contact page  . pleas contact me  if  you   want </h1>")
-     return render(request, 'contact.html')
+    if request.method == "POST":
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        address1 = request.POST.get('address1')
+        address2 = request.POST.get('address2')
+        city = request.POST.get('city')
 
+        # Save the form data to the database
+        contact = Contact(email=email, password=password, address1=address1, address2=address2, city=city)
+        contact.save()
 
+        # Pass a success message to the template
+        return render(request, 'contact.html', {"message": "Your details have been submitted successfully!"})
+
+    # Handle GET request
+    return render(request, 'contact.html')
